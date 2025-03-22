bc83747ad6df89a0a962d2ba1eded9508a52d6b7
N30-geek <dryikobu967@gmail.com>
2025-03-22 02:15:39 +0200

    responsive version1

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 const menu_items = document.querySelectorAll("header menu a");
+const menu_button = document.querySelector(".menu-button");
+const menu = document.querySelector("header menu");
 
+var menu_is_showed = true;
 
 // active clicked menu element
 
@@ -10,4 +13,25 @@ menu_items.forEach(menu => {
         })
         e.target.classList.add("menu-active");
     });
+});
+
+
+// show and hidden menu 
+
+menu_button.addEventListener("click", (e)=>{
+    const icon = document.querySelector("header .menu-button");
+    if(menu_is_showed){
+        
+        menu.style.display = "flex";
+        menu.parentElement.style.height = "max-content";
+        menu_is_showed = false
+
+    }else{
+        
+        menu.style.display = "none";
+        menu.parentElement.style.height = "100px";
+        
+        menu_is_showed = true;
+    }
+
 });
